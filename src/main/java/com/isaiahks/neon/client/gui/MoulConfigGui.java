d661a20c45947d6838c6a0cf2d700460bd5b6705
lightningik <Izayikhan@gmail.com>
2023-10-09 00:11:56 -0700

    commit for computer

---
+++ src/main/java/com/isaiahks/neon/client/gui/MoulConfigGui.java
@@ -0,0 +1,11 @@
+package com.isaiahks.neon.client.gui;
+
+import net.minecraft.client.MinecraftClient;
+
+public class MoulConfigGui {
+    public MoulConfigGui() {
+        MinecraftClient.getInstance().send {
+
+        }
+    }
+}
